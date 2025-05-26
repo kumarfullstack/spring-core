8e720700e9b634f354b7683efd0db88ab5fb9dbe
spring <barunkumar2888@gmail.com>
2025-05-27 00:05:11 +0530

    address bean java file

---
+++ src/main/java/com/data/Address.java
@@ -0,0 +1,22 @@
+package com.data;
+
+public class Address {
+    private String street;
+    private String city;
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
